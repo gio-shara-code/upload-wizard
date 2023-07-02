7f20787cc26451c0c64461facce5039ef1a6f849
gio-shara-code <giorgi.sharashenidze@code.berlin>
2023-07-02 18:17:00 +0200

    chore: added providers/s3

---
+++ packages/providers/s3/src/index.ts
@@ -0,0 +1,2 @@
+console.log('hello world')
+export class S3Provider {}
---
+++ packages/providers/s3/src/s3.ts
@@ -0,0 +1,10 @@
+console.log('Marvin is cool')
+// import { S3Client } from '@aws-sdk/client-s3'
+import config from '../../../image-uploader/src/utils/config'
+
+class S3Bucket {
+    // client: S3Client
+    // constructor() {
+    //     this.client = new S3Client({ region: config.aws.s3.region })
+    // }
+}
