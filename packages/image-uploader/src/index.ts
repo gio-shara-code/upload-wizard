5a18fd976ee13bd246e91160ce911dadcc8f51f5
gio-shara-code <giorgi.sharashenidze@code.berlin>
2023-07-03 07:12:24 +0200

    feat: demo on how to import s3Provider

--- packages/image-uploader/src/index.ts
+++ packages/image-uploader/src/index.ts
@@ -1,2 +1,5 @@
 // NOTE A provider can be imported like this
 import { S3Provider } from '@providers/s3'
+
+const s3Provider = new S3Provider()
+s3Provider.presignedUrl('/mypic.png').then(console.log).catch(console.log)
