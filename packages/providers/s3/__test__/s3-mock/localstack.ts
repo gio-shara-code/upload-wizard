25a56f5b86194548568a9f0c76004b49a50bc109
Marvin Willms <marvin.willms@code.berlin>
2023-07-24 17:01:07 +0200

    test(s3): add s3 mock using localstack and tf

---
+++ packages/providers/s3/__test__/s3-mock/localstack.ts
@@ -0,0 +1,60 @@
+import { spawn, SpawnOptions } from 'child_process'
+
+const childProcessWrapper = (
+    command: string,
+    args?: readonly string[],
+    options?: SpawnOptions
+) => {
+    const childProcess = spawn(command, args, {
+        cwd: __dirname,
+        ...options,
+    })
+
+    return new Promise((resolve, reject) => {
+        childProcess.stdout.on('data', (data) => {
+            process.stdout.write(data.toString())
+        })
+
+        childProcess.stderr.on('data', (data) => {
+            process.stdout.write(data.toString())
+        })
+
+        childProcess.on('close', (code) => {
+            if (code === 0) {
+                return resolve(void 0)
+            } else {
+                return reject(new Error(`exit code ${code}`))
+            }
+        })
+    })
+}
+
+export const setupS3Mock = async () => {
+    try {
+        await childProcessWrapper('make', ['setup'], {
+            timeout: 60 * 3 * 1000,
+        })
+    } catch (e) {
+        throw new Error(`S3 mock setup failed, reason: ${e}`)
+    }
+}
+
+export const teardownS3Mock = async () => {
+    try {
+        await childProcessWrapper('make', ['teardown'], {
+            timeout: 60 * 1000,
+        })
+    } catch (e) {
+        throw new Error(`S3 mock teardown failed, reason: ${e}`)
+    }
+}
+
+export const resetS3Mock = async () => {
+    try {
+        await childProcessWrapper('make', ['s3-reset'], {
+            timeout: 1000,
+        })
+    } catch (e) {
+        throw new Error(`S3 mock reset failed, reason: ${e}`)
+    }
+}
--- packages/providers/s3/__test__/setup-env-vars.ts
+++ packages/providers/s3/__test__/setup-env-vars.ts
@@ -1,2 +1,2 @@
-process.env.AWS_ACCESS_KEY_ID = 'aws_access_key_id'
-process.env.AWS_SECRET_ACCESS_KEY = 'aws_secret_access_key'
+process.env.AWS_ACCESS_KEY_ID = 'test'
+process.env.AWS_SECRET_ACCESS_KEY = 'test'
