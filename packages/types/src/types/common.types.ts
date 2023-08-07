ada07803c29b9370e7b17a9884c1b15e0cc3cfa3
gio-shara-code <giorgi.sharashenidze@code.berlin>
2023-08-07 12:29:02 +0200

    doc(shared-types): small note

--- packages/types/src/types/common.types.ts
+++ packages/types/src/types/common.types.ts
@@ -14,6 +14,7 @@ export const FileStatus = {
 
 export type FileStatusType = keyof typeof FileStatus
 
+// NOTE: Maybe we firstly defined zod object and out of the object generate type. The goal here is to have the highest level of type safety and binding between out ts types and zod objects. Just a thought
 interface File<ID> {
     id: ID
     status: FileStatusType
