0bd4849344bbf522faddfc088540e03e54db0b0d
gio-shara-code <giorgi.sharashenidze@code.berlin>
2023-08-07 12:19:23 +0200

    fix(aws): fixed parsing env variables in aws provider

--- packages/providers/s3/src/config/aws.ts
+++ packages/providers/s3/src/config/aws.ts
@@ -1,20 +1,29 @@
 import { parseEnv } from 'znv'
 import { z } from 'zod'
 
+const schemas = {
+    AWS_S3_BUCKET_NAME: {
+        schema: z.string(),
+    },
+    AWS_S3_BUCKET_REGION: {
+        schema: z.string(),
+    },
+    AWS_ACCESS_KEY_ID: {
+        schema: z.string(),
+    },
+    AWS_SECRET_ACCESS_KEY: {
+        schema: z.string(),
+    },
+}
+
 const {
     AWS_S3_BUCKET_NAME,
     AWS_S3_BUCKET_REGION,
     AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY,
-} = parseEnv(process.env, {
-    AWS_S3_BUCKET_NAME: z.string().optional(),
-    AWS_S3_BUCKET_REGION: z.string().optional(),
+} = parseEnv(process.env, schemas)
 
-    AWS_ACCESS_KEY_ID: z.string(),
-    AWS_SECRET_ACCESS_KEY: z.string(),
-})
-
-export default Object.freeze({
+const config = Object.freeze({
     s3: {
         region: AWS_S3_BUCKET_REGION,
         name: AWS_S3_BUCKET_NAME,
@@ -22,3 +31,5 @@ export default Object.freeze({
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
     accessKeyId: AWS_ACCESS_KEY_ID,
 })
+
+export default config
