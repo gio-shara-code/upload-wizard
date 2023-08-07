77525d58a06c567559d8f39974918edcb347c9a1
gio-shara-code <giorgi.sharashenidze@code.berlin>
2023-08-07 12:23:08 +0200

    fix(route): instead of returning undefined from route they return an empty object

--- apps/examples/nextjs/src/app/api/users/[id]/images/[imageId]/confirm/route.ts
+++ apps/examples/nextjs/src/app/api/users/[id]/images/[imageId]/confirm/route.ts
@@ -1,12 +1,12 @@
-import { NextRequest, NextResponse } from "next/server";
-import { uploadWizard } from "../../../../../../upload-wizard";
+import { NextRequest, NextResponse } from 'next/server'
+import { uploadWizard } from '../../../../../../upload-wizard'
 
 export async function POST(
-  request: NextRequest,
-  { params }: { params: { id: string, imageId: string } }
+    request: NextRequest,
+    { params }: { params: { id: string; imageId: string } }
 ) {
-  // TODO: validate confirm Token
-  await uploadWizard.confirmUpload(params.imageId, 'confirmToken')
+    // TODO: validate confirm Token
+    await uploadWizard.confirmUpload(params.imageId, 'confirmToken')
 
-  return NextResponse.json(undefined)
+    return NextResponse.json({})
 }
--- apps/examples/nextjs/src/app/api/users/[id]/images/[imageId]/route.ts
+++ apps/examples/nextjs/src/app/api/users/[id]/images/[imageId]/route.ts
@@ -1,22 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import { uploadWizard } from "../../../../../upload-wizard";
+import { NextRequest, NextResponse } from 'next/server'
+import { uploadWizard } from '../../../../../upload-wizard'
 
 export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string, imageId: string } }
+    request: NextRequest,
+    { params }: { params: { id: string; imageId: string } }
 ) {
-  // TODO: delete image
-  await uploadWizard.delete(params.imageId)
+    // TODO: delete image
+    await uploadWizard.delete(params.imageId)
 
-  return NextResponse.json(undefined)
+    return NextResponse.json({})
 }
 
 export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string, imageId: string } }
+    request: NextRequest,
+    { params }: { params: { id: string; imageId: string } }
 ) {
-  // TODO: get image
-  await uploadWizard.getData(params.imageId)
+    // TODO: get image
+    await uploadWizard.getData(params.imageId)
 
-  return NextResponse.json(undefined)
+    return NextResponse.json({})
 }
